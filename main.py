4ec1262d1fdd34c45cc759ed9c48caa2ca371ecd
akio kawahara <akawahara.qu@gmailcom>
2023-05-07 13:25:54 +0900

    second commit

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from PIL import Image
 import io
 import base64
 
+def send_image_to_lambda(base64_image):
+    response = requests.post(api_gateway_url, json={"base64_input_image": base64_image})
+    return response.json()
+
 def resize_image(image, size=(256, 256)):
     return image.resize(size, Image.LANCZOS)
 
@@ -18,9 +22,6 @@ def base64_to_image(base64_str):
 
 api_gateway_url = "https://tf10zezfri.execute-api.ap-southeast-2.amazonaws.com/test/processimage"
 
-def send_image_data_to_lambda(base64_image):
-    response = requests.post(api_gateway_url, json={"image_data": base64_image})
-    return response.json()
 
 st.title("Image Upload and Resize Test Application")
 
@@ -39,17 +40,16 @@ if uploaded_file is not None:
     st.write("Base64 Encoded Resized Image:")
     st.text(base64_resized_image)
 
-    # Add this code after st.text(base64_resized_image)
     decoded_image = base64_to_image(base64_resized_image)
     st.write("Decoded Image:")
     st.image(decoded_image)
 
-    lambda_response = send_image_data_to_lambda(base64_resized_image)
+    lambda_response = send_image_to_lambda(base64_resized_image)
     st.write("Lambda Response:")
     st.write(lambda_response)
 
-    if "received_image_data" in lambda_response:
-        received_base64_image = lambda_response["received_image_data"]
+    if "base64_result_image" in lambda_response:
+        received_base64_image = lambda_response["base64_result_image"]
         received_image = base64_to_image(received_base64_image)
         st.write("Received Image from Lambda:")
         st.image(received_image)
